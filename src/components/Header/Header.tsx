f3e6d26951df8fddefa91001dec1cbc74e63d304
Manuel Duran <durbonca@gmail.com>
2021-04-15 14:29:41 -0400

    creating SignIn

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,34 +11,43 @@ export type HeaderProps = {
 const Header = () => {
   const {
     state: { user },
-} = useStateContext()
+  } = useStateContext()
 
   const history = useHistory()
 
   return (
-    <Flex sx={{ justifyContent: 'space-between', alignItems: 'center', bg: 'white', borderBottom: '2px gray solid', p: 3 }} as="nav">
+    <Flex
+      sx={{
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        bg: 'white',
+        borderBottom: '2px gray solid',
+        p: 3,
+      }}
+      as="nav"
+    >
       <Flex
-      onClick={() => {
-        history.push('/')
-        }}
-        sx={{ 
-          ml: 2, 
-          mr: 4, 
-          pr: 4, 
-          color: 'primary', 
-          borderRight: '2px gray solid', 
-          cursor: 'pointer',
-          alignItems: 'center'
-         }}
-        as="h4"
-      >
-      <Image
         onClick={() => {
           history.push('/')
         }}
-        sx={{ width: 50 }}
-        src="/static/logo.jpeg"
-      />  
+        sx={{
+          ml: 2,
+          mr: 4,
+          pr: 4,
+          color: 'primary',
+          borderRight: '2px gray solid',
+          cursor: 'pointer',
+          alignItems: 'center',
+        }}
+        as="h4"
+      >
+        <Image
+          onClick={() => {
+            history.push('/')
+          }}
+          sx={{ width: 50 }}
+          src="/static/logo.jpeg"
+        />
         ASKNIGHTS
       </Flex>
       <Links sx={{ marginX: 2, textDecoration: 'none', color: 'gray' }}>
@@ -57,14 +66,15 @@ const Header = () => {
         <Link to="/">Search</Link>
       </Links>
 
-      {!user?
-      <Links sx={{ marginX: 2, textDecoration: 'none', color: 'gray' }}>
-        <Link to="/SignIn">Sign In</Link>
-      </Links>:
-      <Flex sx={{}} >
-        <UserMenu />
-      </Flex>
-      }
+      {!user ? (
+        <Links sx={{ marginX: 2, textDecoration: 'none', color: 'gray' }}>
+          <Link to="/SignIn">Sign In</Link>
+        </Links>
+      ) : (
+        <Flex sx={{}}>
+          <UserMenu />
+        </Flex>
+      )}
     </Flex>
   )
 }
