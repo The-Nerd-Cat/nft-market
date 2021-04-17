67df8e834c44e6528ff942b9c3e3f105b5670c97
Manuel Duran <durbonca@gmail.com>
2021-04-17 12:58:24 -0400

    Update App.tsx

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -1,57 +1,20 @@
-import { Container, Flex, Button, Spinner, Image } from 'theme-ui'
-import { useWeb3React } from '@web3-react/core'
-import { AbstractConnector } from '@web3-react/abstract-connector'
+import { Container, Flex } from 'theme-ui'
 
 import { Gallery } from '../components'
-import { ActionType, useStateContext } from '../state'
-import { connectorsByName } from '../connectors'
+import { useStateContext } from '../state'
 
 const App = () => {
   const {
-    dispatch,
-    state: { user, activatingConnector },
+    state: { user },
   } = useStateContext()
-  const { connector, activate } = useWeb3React()
+
 
   return (
     <Container>
       {!user ? (
         <>
           <Flex sx={{ justifyContent: 'center' }}>
-            {Object.keys(connectorsByName).map((name: string) => {
-              const currentConnector = connectorsByName[name as keyof typeof connectorsByName]
-              const activating = currentConnector === activatingConnector
-              const connected = currentConnector === connector
-
-              return (
-                <Button
-                  mt={2}
-                  variant="connect"
-                  sx={{
-                    borderColor: activating ? 'orange' : connected ? 'green' : 'unset',
-                    position: 'relative',
-                    maxWidth: 250,
-                  }}
-                  key={name}
-                  onClick={() => {
-                    dispatch({ type: ActionType.SET_CONNECTOR, payload: currentConnector })
-                    activate(
-                      connectorsByName[name as keyof typeof connectorsByName] as AbstractConnector
-                    )
-                  }}
-                >
-                  {currentConnector === connectorsByName.Metamask && (
-                    <Image
-                      sx={{ width: 35, height: 35 }}
-                      mr={3}
-                      src="https://docs.metamask.io/metamask-fox.svg"
-                    />
-                  )}
-                  {name}
-                  {activating && <Spinner size={20} color="white" sx={{ ml: 3 }} />}
-                </Button>
-              )
-            })}
+            Hi! am gonna be a sexy index!
           </Flex>
         </>
       ) : (
